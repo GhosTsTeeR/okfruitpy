90d8b99a64e2968c65f747a517a36fd32e84ccf2
GhosTsTeeR <123junior1sp@gmail.com>
2023-09-27 01:29:40 -0500

    mejorar la parte del codigo anexando conexion a la bd

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,15 @@
 # conexion a la bd
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+  app = Flask(__name__)
+
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://usuario:password@localhost/basedatos'
+  
+  db.init_app(app)
+
+  return app
--- app/app.py
+++ app/app.py
@@ -1,23 +1,59 @@
-from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
-
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy 
+from flask_marshmallow import Marshmallow
+import pymysql
 import __init__
+from . import db
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['SECRET_KEY'] = 'enter-a-very-secretive-key-3479373'
+# inicializar sqlalchemy y marshmallow
+ma = Marshmallow(app)
 
+# modelo de tabla 
+class Usuario(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  nombre = db.Column(db.String(100), unique=True)
+  correo = db.Column(db.String(200))
+  pssw = db.Column(db.String(200))
 
-@app.route('/', methods=["GET", "POST"])
-def index():
-    return render_template('index.html', **locals())
+  def __init__(self, nombre, correo, pssw):
+    self.nombre = nombre
+    self.correo = correo 
+    self.pssw = pssw
 
-@app.route('/api', methods=["GET", "POST"])
-def chatbotResponse():
-    if request.method == 'POST':
-        response = "Peticion a una api"
-    return jsonify({"response": response })
+# schema del usuario
+class UsuarioSchema(ma.Schema):
+  class Meta:
+    fields = ('id', 'nombre', 'correo', 'pssw')
+
+usuario_schema = UsuarioSchema()
+pusuarios_schema = UsuarioSchema(many=True)
+
+# crear un nuevo usuario
+@app.route('/usuario', methods=['POST'])
+def add_usuario():
+  nombre = request.json['nombre']
+  correo = request.json['correo']
+  pssw = request.json['pssw']
+
+  nuevo_usuario = Usuario(nombre, correo, pssw)
+
+  db.session.add(nuevo_usuario)
+  db.session.commit()
+
+  return usuario_schema.jsonify(nuevo_usuario)
+
+# consultar usuarios v:
+@app.route('/productos', methods=['GET']) 
+def get_usuario():
+  all_usuarios = Usuario.query.all()
+  result = usuario_schema.dump(all_usuarios)
+
+  return jsonify(result)
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
