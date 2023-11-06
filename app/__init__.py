d4b40b56890619532beb42918f6f3d638ca3b9a9
lBrayanl <brayan_roasa@fet.edu.co>
2023-11-06 12:38:01 -0500

    Configuraciones

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import bcrypt
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 import mysql.connector
@@ -76,5 +77,50 @@ def add_datos_arandanos(data_json, numero_aleatorio, fecha_actual, version):
     cnx.commit()
     cursor.close()
     cnx.close()
+
+def add_user(correo, password):
+    print(correo, password)
+    try:
+        cnx = get_db_connection()
+        cursor = cnx.cursor()
+        # Generar un hash de la contraseña
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        query = "INSERT INTO Usuario (correo, Tipo_Documento_idTipo_Documento, Rol_idRol, password ) VALUES (%s, %s, %s, %s)"
+        datos = (correo, 1, 1, hashed_password.decode('utf-8'))
+
+        with cnx.cursor() as cursor:
+            cursor.execute(query, datos)
+            cnx.commit()
+
+    except mysql.connector.Error as err:
+        print(f"Error al insertar en MySQL: {err}")
+
+    finally:
+        print("finalizó correctamente")
+        cursor.close()
+        cnx.close()
+
+def login_user(correo, password):
+    try:
+        cnx = get_db_connection()
+        cursor = cnx.cursor()
+        query = "SELECT correo, password FROM Usuario WHERE correo = %s"
+        cursor.execute(query, (correo,))
+        row = cursor.fetchone()
+
+        if row:
+            stored_password = row[1].encode('utf-8')
+            if bcrypt.checkpw(password.encode('utf-8'), stored_password):
+                return 200, row[0] 
+        return 401, ""
+
+    except mysql.connector.Error as err:
+        print(f"Error al verificar en MySQL: {err}")
+        return 500, None
+
+    finally:
+        cursor.close()
+        cnx.close()
+
 
 
